87c3e58017698abd05cdd9640b478afd634648fd
Danniel Sequera <sequeradanniel@gmail.com>
2023-02-07 15:20:05 -0400

    Initial providers configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
+// ** Material UI (@mui)
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+
+// ** Locales
+import ThemeLocalization from './locales';
+
 // ** Custom components
+import SnackbarProvider from './components/snackbar';
+import ScrollToTop from './components/scroll-to-top';
+import { ThemeSettings, SettingsProvider } from './components/settings';
 
 // ** Routes
 import Router from './routes';
@@ -9,9 +19,20 @@ import Router from './routes';
 export default function App() {
   return (
     <HelmetProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <SettingsProvider>
+          <BrowserRouter>
+            <ThemeSettings>
+              <ThemeLocalization>
+                <SnackbarProvider>
+                  <ScrollToTop />
+                  <Router />
+                </SnackbarProvider>
+              </ThemeLocalization>
+            </ThemeSettings>
+          </BrowserRouter>
+        </SettingsProvider>
+      </LocalizationProvider>
     </HelmetProvider>
   );
 }
