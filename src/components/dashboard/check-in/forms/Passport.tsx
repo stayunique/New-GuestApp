3b6fde681af92624297b225b934a7de06dcd622b
Danniel Sequera <sequeradanniel@gmail.com>
2023-02-27 00:04:03 -0400

    NG-16: Passport form implemented

--- src/components/dashboard/check-in/forms/Passport.tsx
+++ src/components/dashboard/check-in/forms/Passport.tsx
@@ -1,7 +1,26 @@
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid, TextField, Typography, Button } from '@mui/material';
+
+// ** Icons
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+
+// ** Third party components
+import { Formik, Form, FieldArray } from 'formik';
 
 // ** Custom components
 import { GuestCard } from './passport/GuestCard';
+
+const initialValues = {
+  guests: [
+    {
+      firstName: '',
+      lastName: '',
+      date: '',
+      passport: '',
+      country: '',
+    },
+  ],
+};
 
 export const Passport = ({ containerProps = {} }) => {
   return (
@@ -16,8 +35,121 @@ export const Passport = ({ containerProps = {} }) => {
         </Typography>
       </Grid>
 
+      {/* Forms */}
       <Grid item xs={12} mt={2}>
-        <GuestCard />
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async values => {
+            await new Promise(r => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          {({ values, handleChange }) => (
+            <Form>
+              <FieldArray name="guests">
+                {({ insert, remove, push }) => (
+                  <>
+                    <Box>
+                      <GuestCard onClick={() => push({ firstName: '', lastName: '', date: '', passport: '', country: '' })} />
+
+                      {values.guests.length > 0 && values.guests.map((guest, i) => <GuestCard guest={guest} sx={{ ...(i === 0 && { ml: 4 }), mr: 4 }} />)}
+                    </Box>
+
+                    {values.guests.length > 0 && (
+                      <>
+                        <Box
+                          mt={5}
+                          sx={{
+                            borderTopLeftRadius: '10px',
+                            borderTopRightRadius: '10px',
+                            bgcolor: '#F4F4F4',
+                            py: 1,
+                            px: 2,
+                            display: 'inline-block',
+                            width: '138.26px',
+                          }}
+                        >
+                          <Box display="flex" alignItems="center" justifyContent="space-around">
+                            <Typography color="secondary" fontWeight="bold">
+                              Guest {values.guests.length}
+                            </Typography>
+                            <PersonIcon color="secondary" />
+                          </Box>
+                        </Box>
+                        <Box
+                          sx={{
+                            // height: '196px',
+                            // width: '1196px',
+                            borderBottomLeftRadius: '10px',
+                            borderBottomRightRadius: '10px',
+                            bgcolor: '#F4F4F4',
+                            p: 5,
+                          }}
+                        >
+                          <Box mb={1}>
+                            <Typography color="secondary" variant="caption">
+                              Enter your information
+                            </Typography>
+                          </Box>
+
+                          {values.guests.map((_, index) => (
+                            <Grid
+                              container
+                              sx={{ ...(index === 0 && { borderTop: '1px solid #337862' }), borderBottom: '1px solid #337862', py: 2 }}
+                              key={index}
+                              // display="flex"
+                              // alignItems="center"
+                              // justifyContent="center"
+                            >
+                              <Grid item xs={12} sm={4} md={3} lg={2} sx={{ mr: { xs: 0, sm: 2 } }}>
+                                <TextField name={`guests.${index}.firstName`} type="text" onChange={handleChange} placeholder="First Name" size="small" value={values.guests[index].firstName} />
+                              </Grid>
+                              <Grid item xs={12} sm={4} md={3} lg={2} sx={{ mr: { xs: 0, sm: 2 } }}>
+                                <TextField name={`guests.${index}.lastName`} type="text" onChange={handleChange} placeholder="Last Name" size="small" value={values.guests[index].lastName} />
+                              </Grid>
+                              <Grid item xs={12} sm={4} md={3} lg={2} sx={{ mr: { xs: 0, sm: 0, md: 2 } }}>
+                                <TextField name={`guests.${index}.date`} type="date" onChange={handleChange} placeholder="Date" size="small" value={values.guests[index].date} />
+                              </Grid>
+                              <Grid item xs={12} sm={4} md={3} lg={2} sx={{ mr: { xs: 0, sm: 2 } }}>
+                                <TextField name={`guests.${index}.passport`} type="text" onChange={handleChange} placeholder="Passport" size="small" value={values.guests[index].passport} />
+                              </Grid>
+                              <Grid item xs={12} sm={4} md={3} lg={2} sx={{ mr: { xs: 0, sm: 2 } }}>
+                                <TextField name={`guests.${index}.country`} type="text" onChange={handleChange} placeholder="Country" size="small" value={values.guests[index].country} />
+                              </Grid>
+                              <Grid item xs={12} sm={4} md={3} lg={1}>
+                                <Button
+                                  variant="contained"
+                                  type="button"
+                                  color="secondary"
+                                  onClick={() => remove(index)}
+                                  sx={{
+                                    height: '46px',
+                                    width: '40px',
+                                    borderRadius: '12.480770111083984px',
+                                    bgcolor: '#8BCCBF',
+                                    opacity: 0.5,
+                                  }}
+                                >
+                                  <DeleteIcon fontSize="large" />
+                                </Button>
+                              </Grid>
+                            </Grid>
+                          ))}
+
+                          <Box mt={1}>
+                            <Typography color="secondary" variant="caption">
+                              *This is the user that will be used as a reference for signing the contract.
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </>
+                    )}
+                  </>
+                )}
+              </FieldArray>
+            </Form>
+          )}
+        </Formik>
       </Grid>
     </Grid>
   );
--- src/components/dashboard/check-in/forms/passport/GuestCard.tsx
+++ src/components/dashboard/check-in/forms/passport/GuestCard.tsx
@@ -1,70 +1,214 @@
 import { Box, Typography, SxProps, useTheme, Button } from '@mui/material';
+
+// ** Icons
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import BookOnlineIcon from '@mui/icons-material/BookOnline';
+import LanguageIcon from '@mui/icons-material/Language';
+
+// ** Custom components
 import SvgColor from '../../../../svg-color/SvgColor';
 
-export const GuestCard = ({ sx = {} }: { sx?: SxProps }) => {
+interface Props {
+  sx?: SxProps;
+  onClick?: () => void;
+  guest?: {
+    firstName: string;
+    lastName: string;
+    date: string;
+    passport: string;
+    country: string;
+  } | null;
+}
+
+export const GuestCard = ({ sx = {}, onClick, guest = null }: Props) => {
   const theme = useTheme();
+  const isGuest = !!guest;
 
   return (
     <Box
       component={Button}
+      onClick={onClick}
       sx={{
         height: '196px',
         width: '346px',
         borderRadius: '15px',
-        bgcolor: '#F4F4F4',
-        boxShadow: '1px 1px 27px rgba(0, 0, 0, 0.25)',
+        bgcolor: isGuest ? '#609B88' : '#F4F4F4',
+        boxShadow: isGuest ? '0px 4px 4px rgba(0, 0, 0, 0.25)' : '1px 1px 27px rgba(0, 0, 0, 0.25)',
         position: 'relative',
         overflow: 'clip',
         transition: theme.transitions.create(['box-shadow'], {
           duration: theme.transitions.duration.shorter,
         }),
         '& .MuiTouchRipple-child': {
-          backgroundColor: `#e0e0e0`,
+          backgroundColor: isGuest ? '#609B88' : `#e0e0e0`,
         },
         '&:hover': {
-          bgcolor: '#F4F4F4',
+          bgcolor: isGuest ? '#609B88' : '#F4F4F4',
           boxShadow: '1px 1px 27px rgba(0, 0, 0, 0.50)',
         },
         ...sx,
       }}
     >
-      <SvgColor
-        src={`/assets/images/backgrounds/forms/passport/bg-new-guest-plus-icon.svg`}
-        sx={{
-          height: '93.15px',
-          width: '108.67px',
-          position: 'absolute',
-          left: '260.62px',
-          top: '18px',
-          color: '#EBEBEB',
-        }}
-      />
+      {isGuest ? (
+        <SvgColor
+          src={`/assets/images/backgrounds/forms/passport/bg-guest-star-icon.svg`}
+          sx={{
+            height: '40px',
+            width: '42px',
+
+            position: 'absolute',
+            left: '82.95%',
+            right: '4.91%',
+            top: '6.63%',
+            bottom: '72.96%',
+
+            color: '#FCBE4F',
+          }}
+        />
+      ) : (
+        <SvgColor
+          src={`/assets/images/backgrounds/forms/passport/bg-new-guest-plus-icon.svg`}
+          sx={{
+            height: '93.15px',
+            width: '108.67px',
+            position: 'absolute',
+            left: '260.62px',
+            top: '18px',
+            color: '#EBEBEB',
+          }}
+        />
+      )}
       <SvgColor
         src={`/assets/images/backgrounds/forms/passport/bg-new-guest.svg`}
         sx={{
           height: '169.9570770263672px',
           width: '169.9570770263672px',
           position: 'absolute',
+
           left: '33.82%',
           right: '17.06%',
           top: '26.7%',
           bottom: '-13.41%',
           color: '#EBEBEB',
+          ...(isGuest && {
+            left: '48.84%',
+            right: '2.04%',
+            top: '20.92%',
+            bottom: '-7.63%',
+            color: '#4F957E',
+          }),
         }}
       />
       <Typography
+        noWrap
         sx={{
+          width: '193px',
           position: 'absolute',
+          fontWeight: '600',
           left: '17px',
           top: '140px',
-          fontWeight: '600',
           fontSize: '24px',
           lineHeight: '28px',
           color: '#B1B1B1',
+          ...(isGuest && {
+            left: '23px',
+            top: '41px',
+            fontSize: '31.4323px',
+            lineHeight: '37px',
+            color: '#FFFFFF',
+          }),
         }}
       >
-        Add new guest
+        {isGuest ? `${guest.firstName || 'N/A'} ${guest.lastName || 'N/A'}` : 'Add new guest'}
       </Typography>
+
+      {isGuest && (
+        <>
+          {/* Date */}
+          <CalendarMonthIcon
+            color="secondary"
+            sx={{
+              position: 'absolute',
+              left: '7.8%',
+              right: '87.86%',
+              top: '44.9%',
+              bottom: '47.17%',
+            }}
+          />
+          <Typography
+            noWrap
+            sx={{
+              position: 'absolute',
+              fontWeight: '400',
+              width: '75px',
+              ml: 1,
+              left: '50px',
+              top: '94px',
+              fontSize: '13px',
+              lineHeight: '15px',
+              color: '#FFFFFF',
+            }}
+          >
+            {guest.date || 'N/A'}
+          </Typography>
+
+          {/* Passport */}
+          <BookOnlineIcon
+            color="secondary"
+            sx={{
+              position: 'absolute',
+              left: '7.8%',
+              right: '83.82%',
+              top: '60%',
+              bottom: '33.77%',
+            }}
+          />
+          <Typography
+            noWrap
+            sx={{
+              width: '75px',
+              position: 'absolute',
+              fontWeight: '400',
+              ml: 1,
+              left: '50px',
+              top: '122px',
+              fontSize: '13px',
+              lineHeight: '15px',
+              color: '#FFFFFF',
+            }}
+          >
+            {guest.passport || 'N/A'}
+          </Typography>
+
+          {/* Country */}
+          <LanguageIcon
+            color="secondary"
+            sx={{
+              position: 'absolute',
+              left: '7.8%',
+              right: '87.57%',
+              top: '75.1%',
+              bottom: '20.41%',
+            }}
+          />
+          <Typography
+            noWrap
+            sx={{
+              width: '75px',
+              position: 'absolute',
+              fontWeight: '400',
+              ml: 1,
+              left: '50px',
+              top: '152px',
+              fontSize: '13px',
+              lineHeight: '15px',
+              color: '#FFFFFF',
+            }}
+          >
+            {guest.country || 'N/A'}
+          </Typography>
+        </>
+      )}
     </Box>
   );
 };
